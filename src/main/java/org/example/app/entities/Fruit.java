89a852789e96080a0ba95d268b591a82420c5d7c
Andriy Svidlo <komandrei5@gmail.com>
2023-04-08 23:40:57 +0300

    Add fruit

--- src/main/java/org/example/app/entities/Fruit.java
+++ src/main/java/org/example/app/entities/Fruit.java
@@ -1,4 +1,20 @@
 package org.example.app.entities;
 
 public class Fruit {
+
+    private final String name;
+    private final String description;
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
