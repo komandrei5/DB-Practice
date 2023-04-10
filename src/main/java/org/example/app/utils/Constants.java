57908571d3f4d6124c67d7fba78bed8d7ba5cdaf
Andriy Svidlo <komandrei5@gmail.com>
2023-04-10 13:40:25 +0300

    Add constants

---
+++ src/main/java/org/example/app/utils/Constants.java
@@ -0,0 +1,14 @@
+package org.example.app.utils;
+
+public class Constants {
+
+    public final static String DB_DRIVER = "jdbc:sqlite:";
+    public final static String DB_BASE_URL = "src/main/resources/database/";
+    public final static String DB_NAME = "fruitsdb.db";
+    public final static String TABLE_NAME = "fruits";
+    public final static String NO_SUCH_OPTION_MSG = "There is no such option! Try again: ";
+    public final static String NAN_NUMBER_MSG = "\"%s\" - not a number! Try again:";
+    public final static String DB_ABSENT_MSG = "\n>> No database!";
+    public final static String DATA_ABSENT_MSG = "\n>> No data.";
+    public final static String APP_CLOSED_MSG = "\n>> App closed.";
+}
